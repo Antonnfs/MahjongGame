129b15ae6e03d1324448d1c94399fd1d5686f461
Anton Pylypenko <90262463+Antonnfs@users.noreply.github.com>
2022-06-12 23:40:16 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,52 +1,83 @@
-
-const cards = 30; // quantity pairs of cards 
+const cards = 30; // quantity pairs of cards;  May be in range beetween 2 and 50;
 const card = document.querySelectorAll('.card');
+
+let mainArray;			// Generated randjm array
+let foundArray = [];		// Array of founded values
 let firstCardValue = '';
 let secondCardValue = '';
-let firstCardItem;
-let secondCardItem;
+let firstCardId;		
+let secondCardId;		
+let firstCardItem;		// First selected card
+let secondCardItem;		// Second selected card
+let selectedPairOfCards = [];		// Array with selected pair of cards
 
 render();
 
+console.log(mainArray);
 window.addEventListener('click', function(e) {
-	let firstCardItem = e.target.closest('.card');
-	let secondCardItem = e.target.closest('.card');
+	firstCardItem = e.target.closest('.card');
+	secondCardItem = e.target.closest('.card');
 	if (e.target.closest('.card')) {
 		if (!firstCardValue && !secondCardValue) {
-			firstCardItem.querySelector('.card__value').classList.toggle("hidden");
-			firstCardValue = e.target.querySelector('.card__value').textContent;
+			firstCardItem.querySelector('.card__value').classList.toggle('hidden');
+			firstCardId = firstCardItem.dataset.id;
+			console.log(`first id: ${firstCardId}`);
+			firstCardValue = +e.target.querySelector('.card__value').textContent;
 			console.log(firstCardValue);
+			selectedPairOfCards.push(firstCardItem);
+			selectedPairOfCards.forEach(function(key) {
+				console.log(key);
+				key.setAttribute('disabled', '');
+				//let child = key.querySelector('p');
+				//console.log(child);
+				//child.setAttribute('disabled', '');
+				})
+			console.log(selectedPairOfCards);
 		} else if (firstCardValue && !secondCardValue) {
-			secondCardItem.querySelector('.card__value').classList.toggle("hidden");
-			secondCardValue = e.target.querySelector('.card__value').textContent;
+			secondCardItem.querySelector('.card__value').classList.toggle('hidden');
+			secondCardId = secondCardItem.dataset.id;
+			console.log(`second id: ${secondCardId}`);
+			secondCardValue = +e.target.querySelector('.card__value').textContent;
 			console.log(secondCardValue);
-		} else if (firstCardValue && secondCardValue) {
+			selectedPairOfCards.push(secondCardItem);
 
-			if (firstCardValue === secondCardValue) {
-				e.target.closest('.card')
+			console.log(selectedPairOfCards);
+			if (firstCardId === secondCardId) {
+				const idList = document.querySelectorAll(`[data-id]`);
+				idList.forEach(function(key) {
+					if (key.dataset.id === secondCardId) {
+						key.classList.add('found');
+					}
+				})
+				console.log(idList);
+				reset();
 
 			} else {
-				firstCardItem.querySelector('.card__value').classList.toggle("hidden");
-				secondCardItem.querySelector('.card__value').classList.toggle("hidden");
+
+				firstCardItem.querySelector('.card__value').classList.remove('hidden');
+				secondCardItem.querySelector('.card__value').classList.remove('hidden');
 				reset();
+				
 			}
+		} else if (firstCardValue && secondCardValue) {
+
+		
 		}
 	}
 })
 
 
 function render() {
-	const array = generateArrayOfPairs();
+	mainArray = generateArrayOfPairs();
 	const wrapper = document.querySelector('.cards-wrapper');
-	for (let i = 0; i < array.length; i++) {
+	for (let i = 0; i < mainArray.length; i++) {
 		wrapper.insertAdjacentHTML('beforeend', `
-		<button class="card" id='${array[i]}'>
-			<p class="card__value hidden">${array[i]}</p>
+		<button class='card' data-id='${mainArray[i]}'>
+			<input type='button' disabled class='card__value hidden'>${mainArray[i]}</input>
 		</button>
 		`)
 	}
 }
-console.log(generateArrayOfPairs());
 
 function generateArrayOfPairs() {
 	let array = [];
@@ -62,7 +93,6 @@ function generateArrayOfPairs() {
 		let arrayOfPairs = [].concat(array, array); 
 		return mixPairs(arrayOfPairs);
 	}
-
 	function mixPairs(array) {
 		for (let i = array.length - 1; i > 0; i--) {
 			let j = Math.floor(Math.random() * (i + 1));
@@ -71,6 +101,7 @@ function generateArrayOfPairs() {
 		return array;
 	}
 }
+
 function reset() {
 	setTimeout(resetItems(), 500)
 	function resetItems() {
@@ -78,6 +109,13 @@ function reset() {
 		secondCardValue = '';
 		firstCardItem = '';
 		secondCardItem = '';
+		console.log(firstCardValue);
+		console.log(secondCardValue);
+		console.log(firstCardItem);
+		console.log(secondCardItem);
+		console.log(`first id: ${firstCardId}`);
+		console.log(`second id: ${secondCardId}`);
+
 	}
-	
+	console.log('reset');
 }
